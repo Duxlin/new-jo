1aca2de82e42b8112febf53fca7b7bd18e91078c
Duxlin <folorunshoabdulquodir1@gmail.com>
2022-09-18 04:35:37 +0100

    jo-md

---
+++ config.js
@@ -0,0 +1,57 @@
+/**
+   * Create By THE JO BOT
+   * Base By Dika
+*/
+
+const fs = require('fs')
+const chalk = require('chalk')
+
+// Website Api
+global.APIs = {
+	zenz: 'https://zenzapis.xyz',
+}
+
+// Apikey Website Api
+global.APIKeys = {
+	'https://zenzapis.xyz': 'Your Key',
+}
+global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+
+global.ownername = 'DUX BOT'
+// Other
+global.owner = ['2347082252014']
+global.premium = ['2347082252014']
+global.packname = 'Hisoka Morrow'
+global.author = 'WhatsApp Bot'
+global.wlcm = []
+global.sessionName = 'session'
+global.prefa = ['','!','.','🐦','🐤','🗿']
+global.sp = '⭔'
+global.mess = {
+    success: '✓ Success',
+    admin: 'Group Admin Special Features!',
+    botAdmin: 'Bot Must Be Admin First!',
+    owner: 'Bot Owner Special Features',
+    group: 'Features Used Only In Groups!',
+    private: 'Feature used only in private chat!',
+    bot: 'Bot Number User Special Features',
+    wait: '𝖑𝖔𝖆𝖉𝖎𝖓𝖌...Please be patient',
+    endLimit: 'Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours',
+}
+global.limitawal = {
+    premium: "Infinity",
+    free: 100
+}
+global.thumb = fs.readFileSync('./lib/jo.jpg')
+global.visoka = { url: 'https://telegra.ph/file/15209657f9d4f59c7ca1e.mp4' }
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
